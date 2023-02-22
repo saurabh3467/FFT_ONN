df3114f728e80caf48dcc5e6182983649bc813f9
Saurabh Soni <saurabh3467@gmail.com>
2023-02-22 20:36:13 +0100

    cleaned-up

--- FFT_test.py
+++ FFT_test.py
@@ -14,35 +14,20 @@ from typing import List
 
 # function to convert comma to dot in a file
 def comma_to_dot(filename: str) -> None:
-    # Open the input file in read mode
-    with open(filename, 'r') as f:
-        # Read all lines of the file and store them in the list 'lines'
-        lines = f.readlines()
-
-    # Open a new file in write mode to write the updated data
-    with open('tmp_data.txt', 'w') as f:
-        # Loop through the lines of the input file
-        for i, line in enumerate(lines):
-            # Skip the first line (header)
-            if i > 0:
-                # Split the line into a list of values
-                data = line.strip().split(';')
-                
-                for j, value in enumerate(data):
-                # Replace commas with dots in the first four values of the line
+    with open(filename, 'r') as f:# Open the input file in read mode
+        lines = f.readlines()# Read all lines of the file and store them in the list 'lines'
+    with open('tmp_data.txt', 'w') as f:# Open a new file in write mode to write the updated data
+        for i, line in enumerate(lines):        # Loop through the lines of the input file
+            if i > 0:# Skip the first line (header)
+                data = line.strip().split(';')# Split the line into a list of values
+                for j, value in enumerate(data):# Replace commas with dots in the all values of the line
                     data[j] = data[j].replace(',', '.')
-                # Join the updated values with semicolons and write them to the new file
-                f.write(';'.join(data) + '\n')
-                #i = i + 1
-                print(i)
+                f.write(';'.join(data) + '\n')# Join the updated values with semicolons and write them to the new file
 
 # function to perform data smoothening on the current-time data
 def data_smoothening(filename: str) -> List[float]:
-    # Load the data from the file using np.loadtxt()
-    data = np.loadtxt(filename, dtype=str, delimiter=';')
-
-    # Extract the current from the data array and Convert from strings to floats
-    current = data[:, 1]
+    data = np.loadtxt(filename, dtype=str, delimiter=';')    # Load the data from the file using np.loadtxt()
+    current = data[:, 1]    # Extract the current from the data array and Convert from strings to floats
     current_float = [float(x) for x in current]
     time = data[:, 0]
     time_float = [float(x) for x in time]
@@ -51,35 +36,22 @@ def data_smoothening(filename: str) -> List[float]:
     # Note: Higher window length -> more smoothing, less detail; higher polynomial order -> more detail, introduced noise
     current_float_smooth = savgol_filter(current_float, 30, 3)
     
-    # Create a figure and axes objects for plotting
-    fig, ax = plt.subplots()
-
-    # Plot the original current-time data in blue
-    ax.plot(current_float, color='blue', label='Original Data')
-
-    # Plot the smoothed data in red
-    ax.plot(current_float_smooth, color='red', label='Smoothed Data')
-
-    # Set the title and axis labels
-    ax.set_title('Current vs. Time')
+    fig, ax = plt.subplots()    # Create a figure and axes objects for plotting
+    ax.plot(current_float, color='blue', label='Original Data')    # Plot the original current-time data in blue
+    ax.plot(current_float_smooth, color='red', label='Smoothed Data')    # Plot the smoothed data in red
+    ax.set_title('Current vs. Time')    # Set the title and axis labels
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Current (A)')
-
-    # Add a legend to the plot
-    ax.legend()
-
-    # Show the plot
-    #plt.show()
-    # Create the subfolder if it does not exist yet
-    if not os.path.exists(os.path.join(path,'Smooth')):
+    ax.legend()    # Add a legend to the plot
+    #plt.show()# Show the plot
+    if not os.path.exists(os.path.join(path,'Smooth')):# Create the subfolder if it does not exist yet
         os.makedirs(os.path.join(path,'Smooth'))
     plt.savefig(os.path.join(path, f'Smooth/{input_filename}_smooth.png'), dpi=300)
     with open(os.path.join(path, f'Smooth/{input_filename}_smooth.txt'), 'w') as f:
         for i in range(len(time_float)):
              f.write('{} {} {}\n'.format(time_float[i], np.abs(current_float[i]), np.abs(current_float_smooth[i])))
-    # Return the smoothed current data as a list of floats
-    return current_float_smooth
-
+    return current_float_smooth# Return the smoothed current data as a list of floats
+    
 # function to perform FFT on the current data
 def perform_fft(current_float_smooth: List[float]) -> List[complex]:
     window = np.hanning(len(current_float_smooth))
@@ -102,27 +74,21 @@ def plot_fft(fft_current: List[complex], time_float: List[float], voltage: int, 
     plt.rcParams['axes.labelweight'] = 'bold'
     plt.legend(['{}V, {} µL, {} cm'.format(voltage, volume, length)])
     
-    # Create the subfolder if it does not exist yet
     if not os.path.exists(os.path.join(path, "FFT")):
         os.makedirs(os.path.join(path, "FFT"))
         
-    # Save the FFT data and plot to a text file in a folder called "FFT"
-    plt.savefig(os.path.join(path, f'FFT/{input_filename}.png'), dpi=300)
+    plt.savefig(os.path.join(path, f'FFT/{input_filename}.png'), dpi=300)# Save the FFT data and plot to a text file in a folder called "FFT"
     with open(os.path.join(path, f'FFT/{input_filename}_FFT.txt'), 'w') as f:
         for i in range(len(freq)):
              f.write('{} {}\n'.format(freq[i], np.abs(fft_current[i])))
         #plt.figure() #shows all plots 1-by-1 instead of just last one
         
-# ========================== functions end here ==============================
+# ========================== functions end here ===============================
 
-# ========================== user input =======================================
-
-# Ask the user for the mass of the droplet and the voltage applied
+# ======== Ask the user for the path & parameters to the directory ============
 volume = float(input("Enter the volume of the droplet in µL: "))
 voltage = float(input("Enter the voltage applied: "))
 length = float(input("Enter the distance between the electrodes in cm: "))
-
-# Ask the user for the path to the directory
 path = input("Enter the path to the directory: ")
 
 smooth = input("Do you want to smoothen I-V data? (y/n)")
@@ -133,13 +99,11 @@ if smooth == 'n':
             input_filename = os.path.splitext(os.path.basename(filename))[0]
             comma_to_dot(os.path.join(path, filename))
             data = np.loadtxt('tmp_data.txt', dtype=str, delimiter=';')
-            # Extract the current from the data array and Convert from strings to floats
             current = data[:, 1]
             current_float = [float(x) for x in current]
             
             fft_result = perform_fft(current_float) # pass the stored value as the input argument to perform_fft
             
-            # Extract the time from the data array and Convert from strings to floats
             time = data[:, 2]
             time_float = [float(x) for x in time]
             plot_fft(fft_result, time_float, voltage, volume, length, path, input_filename)
@@ -153,7 +117,6 @@ elif smooth == 'y':
             fft_result = perform_fft(current_float_smooth) # pass the stored value as the input argument to perform_fft
             
             data = np.loadtxt('tmp_data.txt', dtype=str, delimiter=';')
-            # Extract the time from the data array and Convert from strings to floats
             time = data[:, 2]
             time_float = [float(x) for x in time]
             plot_fft(fft_result, time_float, voltage, volume, length, path, input_filename)
